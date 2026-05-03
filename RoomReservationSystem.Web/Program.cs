ac20b363a6bc12628f129245d2c35f67a10b37d2
marpasdev <marpascze@gmail.com>
2026-05-03 17:38:45 +0200

    Service registration

--- RoomReservationSystem.Web/Program.cs
+++ RoomReservationSystem.Web/Program.cs
@@ -1,5 +1,11 @@
 using Dapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Data.Sqlite;
+using Microsoft.IdentityModel.Tokens;
+using RoomReservationSystem.Web.Repositories;
+using RoomReservationSystem.Web.Services;
+using System.Text;
+
 
 namespace RoomReservationSystem.Web
 {
@@ -9,12 +15,48 @@ namespace RoomReservationSystem.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddScoped<IUserRepository, UserRepository>(
+                _ => new UserRepository(connectionString!));
+            builder.Services.AddScoped<IRoomRepository, RoomRepository>(
+                _ => new RoomRepository(connectionString!));
+            builder.Services.AddScoped<IEquipmentRepository, EquipmentRepository>(
+                _ => new EquipmentRepository(connectionString!));
+            builder.Services.AddScoped<IRoomEquipmentRepository, RoomEquipmentRepository>(
+                _ => new RoomEquipmentRepository(connectionString!));
+            builder.Services.AddScoped<IReservationRepository, ReservationRepository>(
+                _ => new ReservationRepository(connectionString!));
+            builder.Services.AddScoped<IReservationHistoryRepository, ReservationHistoryRepository>(
+                _ => new ReservationHistoryRepository(connectionString!));
+
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<IRoomService, RoomService>();
+            builder.Services.AddScoped<IReservationService, ReservationService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IStatisticsService, StatisticsService>();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+                options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+                    };
+                });
+
             var app = builder.Build();
 
-            string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             using var connection = new SqliteConnection(connectionString);
             var initScript = File.ReadAllText("../Database/init.sql");
             connection.Execute(initScript);
